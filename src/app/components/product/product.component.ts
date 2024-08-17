9268e1810b37a1628d14957f5270ece660c96ab0
Kirill <kirillplasteev@gmail.com>
2024-08-17 20:52:32 +0300

    Final1

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, ViewEncapsulation} from '@angular/core'
 import { IProduct } from '../../moduls/products'
 
 @Component ({
    selector: 'app-product',
-   templateUrl: './product.component.html'
+   templateUrl: './product.component.html',
+   encapsulation: ViewEncapsulation.None
 })
 
 export class ProductComponent {
